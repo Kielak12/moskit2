989b5013adc1c4bb4adfff36ff30652a3339d043
Kielak12 <tymekkielak@gmail.com>
2025-08-22 02:04:41 +0200

    Delete moskitomobil_dynamic_cf_fixed/functions directory

--- moskitomobil_dynamic_cf_fixed/functions/api/me.js
+++
@@ -1,7 +0,0 @@
-import { json, text } from '../lib/utils.js';
-import { checkAuth } from '../lib/auth.js';
-
-export const onRequestGet = async ({ request }) => {
-  if(!checkAuth(request)) return text('Unauthorized', 401);
-  return json({ ok: true });
-};
